f5975da574881a4125a0c8069d74e42bf6819e2f
kyledelacruz1996 <75510831+kyledelacruz1996@users.noreply.github.com>
2020-12-10 11:40:35 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlink = document.getElementsByClassName('navbar-link')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarlink.classList.toggle('active')
-})
